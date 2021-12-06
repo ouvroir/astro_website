27639dc180f84feb579b40950a93e14dbf0b79ba
lenaMK <lena.krause@umontreal.ca>
2021-12-06 20:57:27 +0000

    Deploying to gh-pages from @ ouvroir/site-lab@e64749dbc68069d52e40489954c53b5a1d05881b 🚀

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-export default /** @type {import('astro').AstroUserConfig} */ (
-    {
-        buildOptions: {
-            site: 'ouvroir.github.io',
-            sitemap: true
-        }
-
-    }
-)
