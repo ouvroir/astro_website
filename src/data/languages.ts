934ae6f70c0ec5c5bf520ec4f7e66bf0172c119d
lenamk <lena.krause@umontreal.ca>
2021-12-14 12:12:14 +0100

    languages and related functions

---
+++ src/data/languages.ts
@@ -0,0 +1,23 @@
+import localeSettings from './locales.json';
+const {locales} = localeSettings;
+
+export const DEFAULT_LOCALE = 'fr';
+
+export const KNOWN_LANGUAGES = locales
+  .reduce((accum, l) => {
+    accum[l.language] =  l.locale;
+    return accum
+  }, {});
+
+export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
+export const ALL_LOCALES = locales.map(l => l.locale);
+
+
+export const langPathRegex = /\/([a-z]{2})\//;
+
+export function getLanguageFromURL(pathname: string) {
+  const langCodeMatch = pathname.match(langPathRegex);
+  return langCodeMatch ? langCodeMatch[1] : DEFAULT_LOCALE;
+}
+/* do we need: getLangFromFilename or getSlugFromFilename? */
+/*Inspired by: https://github.com/DefinedNet/nebula-docs/blob/main/src/languages.ts*/
