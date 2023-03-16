d31944e2ec945a172e8977773d4570d7358d7223
William Diakite <william.diakite@gmail.com>
2023-03-16 15:19:59 -0400

    add cieco logo

--- src/data/languages.ts
+++ src/data/languages.ts
@@ -1,11 +1,11 @@
 import localeSettings from './locales.json';
-const {locales} = localeSettings;
+const { locales } = localeSettings;
 
 export const DEFAULT_LOCALE = 'fr';
 
 export const KNOWN_LANGUAGES = locales
     .reduce((accum, l) => {
-        accum[l.language] =  l.locale;
+        accum[l.language] = l.locale;
         return accum
     }, {});
 
@@ -25,137 +25,141 @@ export function getLanguageFromURL(pathname: string) {
 // see https://codepen.io/marcelo-ribeiro/pen/PomqOvE
 
 const accentsMap = new Map([
-  ["-", "\\s|_"],
-  ["a", "á|à|ã|â|ä"],
-  ["e", "é|è|ê|ë"],
-  ["i", "í|ì|î|ï"],
-  ["o", "ó|ò|ô|õ|ö"],
-  ["u", "ú|ù|û|ü"],
-  ["c", "ç"],
-  ["n", "ñ"],
-  ["", "\'"] //retire l'apostrophe, ex: histoire de l'art
-]); 
- 
+    ["-", "\\s|_"],
+    ["a", "á|à|ã|â|ä"],
+    ["e", "é|è|ê|ë"],
+    ["i", "í|ì|î|ï"],
+    ["o", "ó|ò|ô|õ|ö"],
+    ["u", "ú|ù|û|ü"],
+    ["c", "ç"],
+    ["n", "ñ"],
+    ["", "\'"] //retire l'apostrophe, ex: histoire de l'art
+]);
+
 const reducer = (acc, [key]) =>
-  acc.replace(new RegExp(accentsMap.get(key), "gi"), key);
+    acc.replace(new RegExp(accentsMap.get(key), "gi"), key);
 
 //type issue to check
 export const slugify = (text) =>
-  [...accentsMap].reduce(reducer, text.toLowerCase());
+    [...accentsMap].reduce(reducer, text.toLowerCase());
 
 
 /*Inspired by: https://github.com/DefinedNet/nebula-docs/blob/main/src/languages.ts*/
 export const mapGenericToText = {
-    "MENU_SERVICES":{
+    "MENU_SERVICES": {
         fr: "Services",
         en: "Services"
     },
-    "MENU_PROJETS":{
+    "MENU_PROJETS": {
         fr: "Projets",
         en: "Projects"
-    },    
-    "MENU_BLOG":{
+    },
+    "MENU_BLOG": {
         fr: "Actualités",
         en: "News"
     },
-    "TAGS":{
+    "TAGS": {
         fr: "Tags",
         en: "Tags"
     },
-    "LIRE_SUITE":{
+    "LIRE_SUITE": {
         fr: "Continuer la lecture",
         en: "Read more"
     },
-    "PRESENTATION":{
+    "PRESENTATION": {
         fr: "Présentation",
         en: "Presentation"
     },
-    "PRESENTATION_LAB":{
+    "PRESENTATION_LAB": {
         fr: "Présentation du laboratoire",
         en: "Lab Presentation"
     },
-    "SERVICES_DESC":{
+    "SERVICES_DESC": {
         fr: "Services offerts, points de rencontre, équipement commun et code de conduite",
         en: "Facilities, meetups, tools and code of conduct"
     },
-    "PROJETS_DESC":{
+    "PROJETS_DESC": {
         fr: "Présentation de projets et de collaborations du laboratoire",
         en: "Introducing lab projects and collaborations"
     },
-    "ACTUALITES_DESC":{
+    "ACTUALITES_DESC": {
         fr: "Blogue et annonces du lab",
         en: "Presentation"
     },
-    "CREDITS":{ 
+    "CREDITS": {
         fr: "Crédits",
         en: "Credits"
     },
-    "FINANCEMENTS":{ 
+    "FINANCEMENTS": {
         fr: "Financements",
         en: "Funding"
     },
-    "LIRE_PROJET":{
+    "LIRE_PROJET": {
         fr: "En savoir plus",
         en: "Find out more"
     },
-    "TAGLINE":{
+    "TAGLINE": {
         fr: "Un espace ouvert et inclusif",
         en: "An inclusive and open-source space"
     },
-    "SITE_WEB":{
+    "CIECO": {
+        fr: "L’Ouvroir d’histoire de l’art et de muséologie numérique de l’Université de Montréal, est un laboratoire de recherche destiné à soutenir le travail conduit dans le cadre du Partenariat « Des nouveaux usages des collections dans les musées d’art » (CIÉCO) ",
+        en: "L’Ouvroir is a Digital Art History and Museology lab at the Université de Montréal dedicated to support the work conducted within the framework of the Partnership “New Uses of Collections in Art Museums” (CIÉCO)"
+    },
+    "SITE_WEB": {
         fr: "site web",
         en: "website"
     },
-    "PUBLIE_LE":{
+    "PUBLIE_LE": {
         fr: "publié le",
         en: "published on"
     },
-    "PAR":{
+    "PAR": {
         fr: "par",
         en: "by"
     },
-    "INFO":{
+    "INFO": {
         fr: "Informations",
         en: "Information"
     },
-    "A_PROPOS":{
+    "A_PROPOS": {
         fr: "À propos de l'Ouvroir",
         en: "About l'Ouvroir"
     },
-    "LOCALISATION":{
+    "LOCALISATION": {
         fr: 'Localisation',
         en: 'Localization'
-    }, 
-    "RENCONTRE":{
+    },
+    "RENCONTRE": {
         fr: "Points de rencontre",
         en: "Meeeting points"
     },
-    "EQUIPEMENT":{
+    "EQUIPEMENT": {
         fr: "Équipement et locaux",
         en: "Tools and facilities"
     },
-    "CODE":{
+    "CODE": {
         fr: "Code de conduite",
         en: "Code of conduct"
     },
-    "EQUIPE":{
-        fr: 'Équipe',   
+    "EQUIPE": {
+        fr: 'Équipe',
         en: 'Team'
     },
-    "STAFF":{
+    "STAFF": {
         fr: "Personnel",
         en: "Staff"
     },
-    "MEMBER":{
+    "MEMBER": {
         fr: "Membres",
         en: "Members"
     },
-    "INVESTIGATEUR":{
+    "INVESTIGATEUR": {
         fr: "Direction scientifique",
         en: "Lead investigator"
     },
     /*rôles, requiert le pronom*/
-    "DIR_SC":{
+    "DIR_SC": {
         fr: {
             m: "Directeur scientifique",
             f: "Directrice scientifique"
@@ -165,7 +169,7 @@ export const mapGenericToText = {
             f: "Scientific director"
         }
     },
-    "RESP":{
+    "RESP": {
         fr: {
             m: "Responsable du laboratoire",
             f: "Responsable du laboratoire"
@@ -175,7 +179,7 @@ export const mapGenericToText = {
             f: "Lab manager"
         }
     },
-    "INGE":{
+    "INGE": {
         fr: {
             m: "Ingénieur, développeur web",
             f: "Ingénieure, développeuse web"
@@ -185,25 +189,25 @@ export const mapGenericToText = {
             f: "Engineer, web developer"
         }
     },
-    "STUDENT":{
-        fr:{
+    "STUDENT": {
+        fr: {
             m: "Étudiant chercheur",
             f: "Étudiante chercheuse"
         },
-        en:{
+        en: {
             m: "Graduate student member",
             f: "Graduate student member"
         }
     }
-    
-}    
+
+}
 
 //slugs
 //TODO: trouver un moyen de parser _blog et _projets pour récupérer les slugs et les tags
 export const mapGenericToSlug = {
-    "":{
-        fr:"",
-        en:""
+    "": {
+        fr: "",
+        en: ""
     },
     "ACTUALITES": {
         fr: 'actualites',
@@ -214,55 +218,55 @@ export const mapGenericToSlug = {
         fr: 'projets',
         en: 'projects',
     },
-    "SERVICES":{
+    "SERVICES": {
         fr: 'services',
         en: 'services',
     },
-    "INFO":{
+    "INFO": {
         fr: 'lab',
         en: 'lab',
     },
-    "ARTICLE_1":{
+    "ARTICLE_1": {
         fr: 'premier-billet',
         en: 'first-blogpost'
     },
-    "ARTICLE_2":{
+    "ARTICLE_2": {
         fr: 'demo-site',
         en: 'site-demo'
     },
-    "ARTICLE_3":{
+    "ARTICLE_3": {
         fr: 'billet-cieco',
         en: 'cieco-blogpost'
     },
-    "COMMON":{
+    "COMMON": {
         fr: 'common',
         en: 'common'
     },
-    "DIGITALMODELS":{
+    "DIGITALMODELS": {
         fr: 'digital-models-reconstruction-project',
         en: 'digital-models-reconstruction-project'
     },
-    "ENCYCLO":{
+    "ENCYCLO": {
         fr: 'encyclopedie',
         en: 'encyclopedia'
     },
-    "EXPOTS":{
+    "EXPOTS": {
         fr: 'expots',
         en: 'expots'
     },
-    "ICI":{
+    "ICI": {
         fr: 'inscribed-capitals-index',
         en: 'inscribed-capitals-index'
     },
-    "REFRAME":{
+    "REFRAME": {
         fr: 'reframe',
         en: 'reframe'
     },
-    "SITEWEB":{
+    "SITEWEB": {
         fr: 'siteweb',
         en: 'website'
     },
-    "TAGS":{
+    "TAGS": {
         fr: 'tags',
         en: 'tags'
     },
@@ -270,95 +274,95 @@ export const mapGenericToSlug = {
         fr: 'lab',
         en: 'lab'
     },
-    "BLOG":{
+    "BLOG": {
         fr: 'blog',
         en: 'blog'
     },
-    "EVENT":{
+    "EVENT": {
         fr: 'event',
         en: 'event'
     },
-    "PARTENARIAT":{
+    "PARTENARIAT": {
         fr: 'partenariat',
         en: 'partenariat'
     },
-    "JAVASCRIPT":{
+    "JAVASCRIPT": {
         fr: 'javascript',
         en: 'javascript'
     },
-    "CIECO":{
+    "CIECO": {
         fr: 'cieco',
         en: 'cieco'
     },
-    "DOCUMENTATION":{
+    "DOCUMENTATION": {
         fr: 'documentation',
         en: 'documentation'
     },
-    "PARTAGE":{
+    "PARTAGE": {
         fr: 'partage',
         en: 'sharing'
     },
-    "WEB":{
+    "WEB": {
         fr: 'web',
         en: 'web'
     },
-    "MODELISATION3D":{
+    "MODELISATION3D": {
         fr: 'modelisation-3d',
         en: '3d-modeling'
     },
-    "EDITION_NUMERIQUE":{
+    "EDITION_NUMERIQUE": {
         fr: 'edition-numerique',
         en: 'digital-edition'
     },
-    "VISUALISATION3D":{
+    "VISUALISATION3D": {
         fr: 'visualisation-3d',
         en: '3d-visualisation'
     },
-    "FCI":{
-        fr:'fci',
-        en:'fci'
+    "FCI": {
+        fr: 'fci',
+        en: 'fci'
     },
-    "CR":{
-        fr:'cr',
-        en:'cr'
+    "CR": {
+        fr: 'cr',
+        en: 'cr'
     },
-    "HIST_ART_NUMERIQUE":{
-        fr:"histoire-de-l'art-numerique",
-        en:"digital-art-history"
+    "HIST_ART_NUMERIQUE": {
+        fr: "histoire-de-l'art-numerique",
+        en: "digital-art-history"
     },
-    "INTERNATIONAL":{
-        fr:'international',
-        en:'internationl'
+    "INTERNATIONAL": {
+        fr: 'international',
+        en: 'internationl'
     },
-    "SERVEUR":{
-        fr:'serveur',
-        en:'serveur'
+    "SERVEUR": {
+        fr: 'serveur',
+        en: 'serveur'
     },
-    "LENA":{
-        fr: 'lenamk',   
+    "LENA": {
+        fr: 'lenamk',
         en: 'lenamk'
     },
-    "EMMANUEL":{
+    "EMMANUEL": {
         fr: 'emchateau',
         en: 'emchateau'
     },
-    "KRISTINE":{
+    "KRISTINE": {
         fr: 'ktanton',
         en: 'ktanton'
     },
-    "WILLIAM":{
+    "WILLIAM": {
         fr: 'williamdiakite',
         en: 'williamdiakite'
     },
-     "JESSICA":{
+    "JESSICA": {
         fr: 'gdJESS',
         en: 'gdJESS'
     },
-    "LEA":{
+    "LEA": {
         fr: 'LeaMaronet',
         en: 'LeaMaronet'
     },
-    "TALITHA":{
+    "TALITHA": {
         fr: 'talithamotter',
         en: 'talithamotter'
     }
@@ -373,7 +377,7 @@ export const mapSlugToGeneric = Object.keys(mapGenericToSlug).reduce((acc, k) =>
 }, {})
 
 
-export function formatDate (date, lang){
+export function formatDate(date, lang) {
     var bonneDate;
     //enlève l'heure et time zone
     date = date.replace('T00:00:00.000Z', '')
@@ -385,107 +389,107 @@ export function formatDate (date, lang){
 
     //enlève le 0 devant les jours de 1 à 9
     if (jour.charAt(0) == "0")
-    jour = jour.slice(1)
+        jour = jour.slice(1)
 
     if (mois.charAt(0) == "0")
-    mois = mois.slice(1)
+        mois = mois.slice(1)
 
-    if (lang == "en"){
-        switch (parseInt(mois)){
-            case 1: 
+    if (lang == "en") {
+        switch (parseInt(mois)) {
+            case 1:
                 moisTxt = "January";
                 break;
-            case 2: 
+            case 2:
                 moisTxt = "February";
                 break;
             case 3: moisTxt = "March";
                 break;
-            case 4: 
+            case 4:
                 moisTxt = "April";
                 break;
-            case 5: 
+            case 5:
                 moisTxt = "May";
                 break;
-            case 6: 
+            case 6:
                 moisTxt = "June";
                 break;
-            case 7: 
+            case 7:
                 moisTxt = "July";
                 break;
-            case 8: 
+            case 8:
                 moisTxt = "August";
                 break;
-            case 9: 
+            case 9:
                 moisTxt = "September";
                 break;
-            case 10: 
+            case 10:
                 moisTxt = "October";
                 break;
-            case 11: 
+            case 11:
                 moisTxt = "November";
                 break;
-            case 12: 
+            case 12:
                 moisTxt = "December";
-                break;   
-            default: console.log(mois, 'error')         
+                break;
+            default: console.log(mois, 'error')
         }
 
-        switch (parseInt(jour.slice(-1))){
-            case 1: 
+        switch (parseInt(jour.slice(-1))) {
+            case 1:
                 bonneDate = moisTxt.concat(` ${jour}st, ${annee}`);
-                break;   
-            case 2: 
+                break;
+            case 2:
                 bonneDate = moisTxt.concat(` ${jour}nd, ${annee}`);
-                break;   
-            case 3: 
+                break;
+            case 3:
                 bonneDate = moisTxt.concat(` ${jour}rd, ${annee}`);
                 break;
-            default: 
+            default:
                 bonneDate = moisTxt.concat(` ${jour}th, ${annee}`);
         }
     }
-    else if (lang == "fr"){
-        switch (parseInt(mois)){
-            case 1: 
+    else if (lang == "fr") {
+        switch (parseInt(mois)) {
+            case 1:
                 moisTxt = "janvier";
                 break;
-            case 2: 
+            case 2:
                 moisTxt = "février";
                 break;
-            case 3: 
+            case 3:
                 moisTxt = "mars";
                 break;
-            case 4: 
+            case 4:
                 moisTxt = "avril";
                 break;
-            case 5: 
+            case 5:
                 moisTxt = "mai";
                 break;
-            case 6: 
+            case 6:
                 moisTxt = "juin";
                 break;
-            case 7: 
+            case 7:
                 moisTxt = "juillet";
                 break;
-            case 8: 
+            case 8:
                 moisTxt = "août";
                 break;
-            case 9: 
+            case 9:
                 moisTxt = "septembre";
                 break;
-            case 10: 
+            case 10:
                 moisTxt = "octobre";
                 break;
-            case 11: 
+            case 11:
                 moisTxt = "novembre";
                 break;
-            case 12: 
+            case 12:
                 moisTxt = "décembre";
-                break;            
+                break;
             default: console.log(mois, 'error')
         }
-        switch (parseInt(jour)){
-            case 1: 
+        switch (parseInt(jour)) {
+            case 1:
                 bonneDate = jour.concat(`er ${moisTxt} ${annee}`);
                 break;
             default: bonneDate = jour.concat(` ${moisTxt} ${annee}`);
